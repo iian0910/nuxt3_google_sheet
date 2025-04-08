776ef566d863b3a0f1550f3e1f9c48f473c60329
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2025-04-08 13:54:36 +0800

    將 NuxtImg 標籤改為 img 標籤,處理assets下圖檔無法顯示問題

--- app.vue
+++ app.vue
@@ -1,3 +1,48 @@
+<script setup>
+  import logo from '~/assets/images/logo.png'
+
+  const config = useRuntimeConfig()
+  const apiKey = config.public.NUXT_API_KEY
+  const sheetId = config.public.NUXT_SHEET_ID
+
+  const sheetData = ref([])
+
+  onMounted(() => {
+    initSheetContent()
+  })
+
+  const initSheetContent = () => {
+    const range = "工作表1!A1:M1000";
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        transData(data.values)
+      })
+      .catch((error) => console.error("Error:", error))
+  }
+
+  const transData = (data) => {
+    // console.log('first')
+    const keys = data[0] // 取出標題列
+    const formattedData = data.slice(1).map(row => {
+      return Object.fromEntries(keys.map((key, index) => [key, row[index]]))
+    })
+
+    sheetData.value = formattedData
+  }
+
+  const keyIn = () => {
+    navigateTo('https://docs.google.com/spreadsheets/d/1FaS9wO-zK4kT_oAuKBo5KWaWa8_ENaMf9vA8tiDK_q0/edit?gid=0#gid=0', {
+      external: true,
+      open: {
+        target: '_blank'
+      }
+    })
+  }
+</script>
+
 <template>
   <div>
     <div class="my-6 d-flex justify-center">
@@ -19,7 +64,7 @@
         <div class="d-flex">
           <div class="main pr-3 py-5">
             <div class="mb-3">
-              <NuxtImg :src="logo" width="230"/>
+              <img :src="logo" width="230"/>
             </div>
             <div class="mb-5 font-weight-bold">{{ ticket.LASTNAME }} / {{ticket.FIRSTNAME}} {{ ticket.GENDER }}</div>
             <div class="d-flex justify-space-between mb-8">
@@ -51,7 +96,12 @@
             <div class="font-weight-bold">PLEASE BE AT THE BOARDING GATE 30 MINS PRIOR TO DEPARTURE. GATE CLOSES 10 MINS BEFORE.</div>
           </div>
           <div class="sub pl-3 py-5">
-            <div class="text-h6 font-weight-bold mb-2">PREMIUM ECONOMY</div>
+            <!--
+              first class     頭等艙
+              business class  商務艙
+              economy class   經濟艙
+            -->
+            <div class="text-h6 font-weight-bold mb-2">{{ ticket.CLASS }}</div>
             <div class="mb-5 font-weight-bold">{{ ticket.LASTNAME }} / {{ticket.FIRSTNAME}} {{ ticket.GENDER }}</div>
             <div class="d-flex justify-space-between">
               <div>
@@ -89,51 +139,6 @@
     </div>
   </div>
 </template>
-
-<script setup>
-  import logo from '~/assets/images/logo.png'
-
-  const config = useRuntimeConfig()
-  const apiKey = config.public.NUXT_API_KEY
-  const sheetId = config.public.NUXT_SHEET_ID
-
-  const sheetData = ref([])
-
-  onMounted(() => {
-    initSheetContent()
-  })
-
-  const initSheetContent = () => {
-    const range = "工作表1!A1:L1000";
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        transData(data.values)
-      })
-      .catch((error) => console.error("Error:", error))
-  }
-
-  const transData = (data) => {
-    // console.log('first')
-    const keys = data[0] // 取出標題列
-    const formattedData = data.slice(1).map(row => {
-      return Object.fromEntries(keys.map((key, index) => [key, row[index]]))
-    })
-
-    sheetData.value = formattedData
-  }
-
-  const keyIn = () => {
-    navigateTo('https://docs.google.com/spreadsheets/d/1FaS9wO-zK4kT_oAuKBo5KWaWa8_ENaMf9vA8tiDK_q0/edit?gid=0#gid=0', {
-      external: true,
-      open: {
-        target: '_blank'
-      }
-    })
-  }
-</script>
 
 <style scoped lang="scss">
 .ticket_content {

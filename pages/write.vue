a3349250ac4c78ee6520770f9433b612c5d9f115
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2025-04-10 14:50:38 +0800

    調整資料輸入頁面排版

--- pages/write.vue
+++ pages/write.vue
@@ -8,93 +8,130 @@
     </v-btn>
     
   </div>
-  <v-sheet class="mx-auto my-6" width="300">
-
+  <v-sheet class="mx-auto my-6" width="1200">
     <v-form ref="form">
-      <v-text-field
-        v-model="firstName"
-        :rules="[v => !!v || 'FirstName is required']"
-        label="FIRST NAME"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="lastName"
-        :rules="[v => !!v || 'Last name is required']"
-        label="LAST NAME"
-        required
-      ></v-text-field>
-      <v-select
-        v-model="gender"
-        :items="genderItem"
-        item-title="name"
-        item-value="value"
-        :rules="[v => !!v || 'Gender is required']"
-        label="GENDER"
-        required
-      ></v-select>
-      <v-text-field
-        v-model="flight"
-        :rules="[v => !!v || 'Flight is required']"
-        label="FLIGHT"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="boardingTime"
-        :rules="[v => !!v || 'Boarding time is required']"
-        label="BOARDING TIME"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="gate"
-        :rules="[v => !!v || 'Gate is required']"
-        label="GATE"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="seat"
-        :rules="[v => !!v || 'Seat is required']"
-        label="SEAT"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="fromWhere"
-        :rules="[v => !!v || 'Item is required']"
-        label="FORM"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="toWhere"
-        :rules="[v => !!v || 'Item is required']"
-        label="TO"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="date"
-        :rules="[v => !!v || 'Date is required']"
-        label="DATE"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="departure"
-        :rules="[v => !!v || 'Departure is required']"
-        label="DEPARTURE"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="terminal"
-        :rules="[v => !!v || 'Terminal is required']"
-        label="TERMINAL"
-        required
-      ></v-text-field>
-      <v-select
-        v-model="flightClass"
-        :items="flightItem"
-        item-title="name"
-        item-value="value"
-        :rules="[v => !!v || 'Flight class is required']"
-        label="FLIGHT CLASS"
-        required
-      ></v-select>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="firstName"
+              :rules="[v => !!v || 'FirstName is required']"
+              label="FIRST NAME"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="lastName"
+              :rules="[v => !!v || 'Last name is required']"
+              label="LAST NAME"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-select
+              v-model="gender"
+              :items="genderItem"
+              item-title="name"
+              item-value="value"
+              :rules="[v => !!v || 'Gender is required']"
+              label="GENDER"
+              required
+            ></v-select>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="flight"
+              :rules="[v => !!v || 'Flight is required']"
+              label="FLIGHT"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="boardingTime"
+              :rules="[v => !!v || 'Boarding time is required']"
+              label="BOARDING TIME"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="gate"
+              :rules="[v => !!v || 'Gate is required']"
+              label="GATE"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="seat"
+              :rules="[v => !!v || 'Seat is required']"
+              label="SEAT"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="fromWhere"
+              :rules="[v => !!v || 'Item is required']"
+              label="FORM"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="toWhere"
+              :rules="[v => !!v || 'Item is required']"
+              label="TO"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="date"
+              :rules="[v => !!v || 'Date is required']"
+              label="DATE"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="departure"
+              :rules="[v => !!v || 'Departure is required']"
+              label="DEPARTURE"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="terminal"
+              :rules="[v => !!v || 'Terminal is required']"
+              label="TERMINAL"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-select
+              v-model="flightClass"
+              :items="flightItem"
+              item-title="name"
+              item-value="value"
+              :rules="[v => !!v || 'Flight class is required']"
+              label="FLIGHT CLASS"
+              required
+            ></v-select>
+          </v-col>
+        </v-row>
+      </v-container>
 
       <div class="d-flex flex-column">
         <v-btn
